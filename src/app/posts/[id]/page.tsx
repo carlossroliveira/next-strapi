44aa12af7fac3e442e2c998f709e9118c75b188e
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-03-14 16:33:13 -0300

    feat: implementation generateStaticParams

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,10 +2,10 @@ import axios from 'axios'
 import { Article } from '../types'
 import ArticleContent from './ArticleContent'
 
+const STRAPI_URL: string = 'http://127.0.0.1:4000'
+
 export default async function PageId({ params }: { params: { id: string } }) {
   const { id } = params
-
-  const STRAPI_URL: string = 'http://127.0.0.1:4000'
 
   const response = await axios.get(`${STRAPI_URL}/api/artigos/${id}`, {
     params: { populate: '*' }
@@ -14,4 +14,16 @@ export default async function PageId({ params }: { params: { id: string } }) {
   const article: Article = response?.data?.data
 
   return <ArticleContent article={article} />
+}
+
+export const generateStaticParams = async () => {
+  const response = await axios.get(`${STRAPI_URL}/api/artigos`, {
+    params: { populate: '*' }
+  })
+
+  return response.data.data.map(
+    (article: { documentId: { toString: () => string } }) => ({
+      documentId: article.documentId.toString()
+    })
+  )
 }
